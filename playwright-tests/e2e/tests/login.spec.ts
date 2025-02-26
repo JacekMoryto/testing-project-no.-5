12ffe75d7fc298c9fc4b97117fd5c602441aed23
JacekMoryto <jacekmateusz91@gmail.com>
2025-02-26 12:48:28 +0100

    Added tests for login

---
+++ playwright-tests/e2e/tests/login.spec.ts
@@ -0,0 +1,21 @@
+import {test,expect} from '@playwright/test'
+
+
+test.describe('Login Page', () => {
+
+    test('Login with valid credentials', async ({page}) => {
+
+        await page.goto('http://localhost:3000/')
+        await page.getByTestId('login-email-field').fill("oliver@example.com");
+        await page.getByTestId('login-password-field').fill("welcome");
+        await page.getByTestId('login-submit-button').click();
+
+        await expect(page.getByTestId('navbar-username-label')).toBeVisible();
+        await expect(page.getByTestId('navbar-logout-link')).toBeVisible();
+
+
+
+    })
+
+
+})
